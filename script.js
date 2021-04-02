12fb44a98ba9621a98294bacd2ac717b65c53523
pages <maksim.krasovskii@yandex.ru>
2021-04-02 21:13:04 +0300

    Домашняя работа к Lesson-2

--- script.js
+++ script.js
@@ -1,23 +1,103 @@
+//Ответ на задание 3:
 
-function func1() {
-    var celsius = 7
-    var fahrenheit = (9 / 5) * celsius + 32;
+		/*var a = -1;
+		var b = 9;
 
-    fahrenheit = Math.floor(fahrenheit);
+        
+		if (a > 0 && b > 0){
+		x = a - b;
+        alert(x + ' если a и b положительные это их разность');
+        }
+        
+
+         else if (a < 0 && b < 0){
+			x = a * b;
+			alert (x + ' произведение этих чисел если а и b Меньше 0');
+			}
+		  else if (a > 0 && b < 0 || a < 0 && b > 0) {
+		  x = a + b;
+		  alert (x + ' если а и b разных знаков, вывести их сумму; ноль можно считать положительным числом.') ;
+		  }*/
 
 
-    alert('Сегодня ' + celsius + 'градусов по цельсию и ' + fahrenheit + 'по фаренгейту ');
-}
 
-func1();
 
-function func2() {
-    var admin = null;
-    var name = 'Василий';
 
-    var admin = admin + name;
+//Задание № 4
+		/*a = +prompt ('Ответ на задание № 4 Начинайте вводить числа от 1 до 15');
+       
+		switch (a) {
+		case 1:
+			alert ( 'Вы ввели число: 1');
+			break;
+		case 2:
+			alert ( 'Вы ввели число: 2');
+			break;
+		case 3:
+			alert ( 'Вы ввели число: 3');
+			break;
+		case 4:
+			alert ( 'Вы ввели число: 4');
+			break;
+		case 5:
+			alert ( 'Вы ввели число: 5');
+			break;
+		case 6:
+			alert ( 'Вы ввели число: 6');
+			break;
+		case 7:
+			alert ( 'Вы ввели число: 7');
+			break;
+		case 8:
+			alert ( 'Вы ввели число: 8');
+			break;
+		case 9:
+			alert ( 'Вы ввели число: 9');
+			break;
+		case 10:
+			alert ( 'Вы ввели число: 10');
+			break;
+		case 11:
+			alert ( 'Вы ввели число: 11');
+			break;
+		case 12:
+			alert ( 'Вы ввели число: 12');
+			break;
+		case 13:
+			alert ( 'Вы ввели число: 13');
+			break;
+		case 14:
+			alert ( 'Вы ввели число: 14');
+			break;
+		case 15:
+			alert ( 'Вы ввели число: 15');
+			break;	
+		}*/
+		
+        
+        
+        
+        // Задание № 5
+        /*var a = 7;
+		var b = 8;
 
-    alert(name);
-}
+		function addition(a, b) {
+    		return  a + b;
+            
+		}
 
-func2();
+		function minus(a, b) {
+    		return a - b;
+		}
+
+		function division (a, b) {
+    		return a / b;
+		}
+
+		function multiplication (a, b) {
+    		return a * b;
+		}
+       
+
+       
+    } */
